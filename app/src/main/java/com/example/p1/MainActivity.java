8a5679bbd9a66f0d1b58625de48c2743e239a010
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2021-05-30 16:54:16 -0400

    upd second view formatting

--- app/src/main/java/com/example/p1/MainActivity.java
+++ app/src/main/java/com/example/p1/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.OnC
     private List<String> mCourseNames;
     private List<String> courseNames;
 
+    private List<String> mCourseDescripts;
+    private List<String> courseDescripts;
+
     private List<List<String>> mPreReqs;
     private List<List<String>> preReqs;
 
@@ -51,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.OnC
 
                 courseTitles = new ArrayList<>();
                 courseNames = new ArrayList<>();
+                courseDescripts = new ArrayList<>();
                 creditList = new ArrayList<>();
                 preReqs = new ArrayList<>();
 
@@ -60,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.OnC
                     if (item.contains(input)) {
                         courseTitles.add(mCourseTitles.get(j));
                         courseNames.add(mCourseNames.get(j));
+                        courseDescripts.add(mCourseDescripts.get(j));
                         creditList.add(mCreditList.get(j));
                         preReqs.add(mPreReqs.get(j));
                     }
@@ -81,63 +86,81 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.OnC
         //init master
         mCourseTitles = new ArrayList<>();
         mCourseNames = new ArrayList<>();
+        mCourseDescripts = new ArrayList<>();
         mCreditList = new ArrayList<>();
         mPreReqs = new ArrayList<>();
 
         //init copy
         courseTitles = new ArrayList<>();
         courseNames = new ArrayList<>();
+        courseDescripts = new ArrayList<>();
         creditList = new ArrayList<>();
         preReqs = new ArrayList<>();
 
         //insert elements to master
         mCourseTitles.add("CS1100");
         mCourseNames.add("Freshman Leap Seminar");
+        mCourseDescripts.add("Small group discussions with " +
+                "first year students are led by one or more faculty members and include a variety of foundational, " +
+                "motivational, and topical subjects for computationalist.");
         mCreditList.add(1);
         mPreReqs.add(new ArrayList<String>());
         mPreReqs.get(0).add("No Prerequisite courses");
 
         mCourseTitles.add("CS1301");
         mCourseNames.add("Introduction to Computing");
+        mCourseDescripts.add("Introduction to computing principles and " +
+                "programming practices with an emphasis on the design, " +
+                "construction and implementation of problem solutions use of software tools.");
         mCreditList.add(3);
         mPreReqs.add(new ArrayList<String>());
         mPreReqs.get(1).add("No Prerequisite courses");
 
         mCourseTitles.add("CS1331");
         mCourseNames.add("Introduction to Object Oriented Programming");
+        mCourseDescripts.add("Introduction to techniques and methods of object-oriented programming such an encapsulation, " +
+                "inheritance, and polymorphism. " +
+                "Emphasis on software development and individual programming skills.");
         mCreditList.add(3);
         mPreReqs.add(new ArrayList<String>());
         mPreReqs.get(2).add("CS1301");
 
         mCourseTitles.add("CS1332");
         mCourseNames.add("Datacourses Structures and Algorithms for Applications");
+        mCourseDescripts.add("Computer data structures and algorithms in the context " +
+                "of object-oriented programming. Focus on software development towards applications.");
         mCreditList.add(3);
         mPreReqs.add(new ArrayList<String>());
         mPreReqs.get(3).add("CS1331");
 
         mCourseTitles.add("CS2050");
         mCourseNames.add("Introduction to Discrete Mathematics for Computer Science");
+        mCourseDescripts.add("Proof methods, strategy, correctness of algorithms over discrete structures. " +
+                "Induction and recursion. Complexity and order of growth. Number theoretic principles and algorithms. Counting and computability.");
         mCreditList.add(3);
         mPreReqs.add(new ArrayList<String>());
         mPreReqs.get(4).add("No Prerequisite courses");
 
         mCourseTitles.add("CS2110");
         mCourseNames.add("Computer Organization and Programming");
+        mCourseDescripts.add("An introduction to basic computer hardware, machine language, assembly language, and C programming.");
         mCreditList.add(4);
         mPreReqs.add(new ArrayList<String>());
         mPreReqs.get(5).add("CS1332");
 
         mCourseTitles.add("CS2340");
         mCourseNames.add("Objects and Design");
+        mCourseDescripts.add("Object-oriented programming methods for dealing with large programs. " +
+                "Focus on quality processes, effective debugging techniques, and testing to assure a quality product.");
         mCreditList.add(4);
         mPreReqs.add(new ArrayList<String>());
-        //to be added
-        mPreReqs.get(6).add("");
+        mPreReqs.get(6).add("CS 1331 or 1372 or 1316");
 
 
         //transfer to copy
         courseTitles.addAll(mCourseTitles);
         courseNames.addAll(mCourseNames);
+        courseDescripts.addAll(mCourseDescripts);
         creditList.addAll(mCreditList);
         preReqs.addAll(mPreReqs);
 
@@ -157,6 +180,7 @@ public class MainActivity extends AppCompatActivity implements CustomAdapter.OnC
         Bundle bundle = new Bundle();
         bundle.putString("course_title", courseTitles.get(position));
         bundle.putString("course_name", courseNames.get(position));
+        bundle.putString("course_descript", courseDescripts.get(position));
         bundle.putInt("credit", creditList.get(position));
         bundle.putStringArrayList("prereq_list", (ArrayList<String>) preReqs.get(position));
 
--- app/src/main/java/com/example/p1/second_activity.java
+++ app/src/main/java/com/example/p1/second_activity.java
@@ -12,11 +12,13 @@ public class second_activity extends AppCompatActivity {
     private Bundle bundle;
     private String courseTitle;
     private String courseName;
+    private String courseDescript;
     private int creditAmoount;
     private List<String> preReqList;
 
     private TextView mCourseTitle;
     private TextView mCourseName;
+    private TextView mCourseDescript;
     private TextView mCreditAmount;
     private TextView mPreReqList;
 
@@ -30,17 +32,20 @@ public class second_activity extends AppCompatActivity {
         if (bundle != null) {
             courseTitle = bundle.getString("course_title");
             courseName = bundle.getString("course_name");
+            courseDescript = bundle.getString("course_descript");
             creditAmoount = bundle.getInt("credit");
             preReqList = bundle.getStringArrayList("prereq_list");
         }
 
         mCourseTitle= findViewById(R.id.title);
-        mCourseName= findViewById(R.id.name);
+        mCourseName = findViewById(R.id.name);
+        mCourseDescript = findViewById(R.id.descript_text);
         mCreditAmount = findViewById(R.id.credit_text);
         mPreReqList = findViewById(R.id.prereq_text);
 
         mCourseTitle.setText(courseTitle);
         mCourseName.setText(courseName);
+        mCourseDescript.setText(courseDescript);
         mCreditAmount.setText(creditAmoount + "");
         String ss1 = "";
         for (int i = 0; i < preReqList.size(); ++i) {
